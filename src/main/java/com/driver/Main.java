bc52ee02c6d8416ae30f106b0ba98c281a74839c
Rohit-6320 <konduskarrohit@gmail.com>
2023-09-03 23:41:42 +0530

    Task 5

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,9 +5,11 @@ public class Main {
     public static void main(String args[])
     {
         Product p=new Product();
-        int ans=p.product(1,3);
+        System.out.println(p.product(1,3));
 
-        int ans2=p.product(1,2,3);
+        System.out.println(p.product(1,2,3));
+
+        System.out.println(p.product(1.2,2.2));
     }
 
 }
